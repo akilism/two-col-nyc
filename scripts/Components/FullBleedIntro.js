ad3ae94011a573c713607a3783a4f9da9bd3524d
Erik Hinton <erik.hinton@vice.com>
2015-12-28 16:29:43 -0500

    deep links

--- scripts/Components/FullBleedIntro.js
+++ scripts/Components/FullBleedIntro.js
@@ -24,18 +24,18 @@ export default class FullBleedIntro extends ScanComponent {
                 <h3 className="title">VICE GUIDE TO NEW YORK</h3>
                 <div className="toc">
                     <ul className="toc-l">
-                        <li><em>1. </em> Neighborhoods</li>
-                        <li><em>2. </em>Etiquette</li>
-                        <li><em>3. </em>Where to Drink/Party</li>
-                        <li><em>4. </em>Where to Eat</li>
-                        <li><em>5. </em>Transcendence</li>
+                        <li><a href="#Neighborhoods"><em>1. </em> Neighborhoods</a></li>
+                        <li><a href="#Etiquette"><em>2. </em>Etiquette</a></li>
+                        <li><a href="#Drink"><em>3. </em>Where to Drink/Party</a></li>
+                        <li><a href="#Eat"><em>4. </em>Where to Eat</a></li>
+                        <li><a href="#Transcendence"><em>5. </em>Transcendence</a></li>
                     </ul>
                     <ul className="toc-r">
-                        <li><em>6. </em>Sports</li>
-                        <li><em>7. </em>What to Do During the Day</li>
-                        <li><em>8. </em>Music</li>
-                        <li><em>9. </em>Shopping</li>
-                        <li><em>10. </em>Getting Around</li>
+                        <li><a href="#Sports"><em>6. </em>Sports</a></li>
+                        <li><a href="#Daytime"><em>7. </em>What to Do During the Day</a></li>
+                        <li><a href="#Music"><em>8. </em>Music</a></li>
+                        <li><a href="#Shopping"><em>9. </em>Shopping</a></li>
+                        <li><a href="#Around"><em>10. </em>Getting Around</a></li>
                     </ul>
                 </div>
             </div>
